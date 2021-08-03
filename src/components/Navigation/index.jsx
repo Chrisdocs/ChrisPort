f61caf3f123ae5f059962eec5028c252d99d220b
Chrisdocs <chrisa.wolfe@gmail.com>
2021-08-03 18:59:30 -0500

    navbar links and DOM structure, css

---
+++ src/components/Navigation/index.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import '../../assets/css/nav.css';
+
+function Navigation(props) {
+
+    return (
+        <section className="nav-section">
+            <div className="container">
+            <div className="row align-items-start">
+                <div className="col">
+                {" "}
+                    <a href="/" data-testid="about" className="nav-link">
+                        About
+                    </a>
+                </div>
+                <div className="col">
+                {" "}
+                    <a href="/" data-testid="projects" className="nav-link">
+                        Projects
+                    </a>
+                </div>
+                <div className="col">
+                    <a href="/" data-testid="contact" className="nav-link">
+                        Contact
+                    </a>
+                </div>
+            </div>
+            </div>
+        </section>
+    );
+}
+
+export default Navigation;
