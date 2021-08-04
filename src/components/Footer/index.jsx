e34c024d2944f737f73b3af4d8f808e81a2245be
Chrisdocs <chrisa.wolfe@gmail.com>
2021-08-03 19:16:25 -0500

    footer DOM, positioning, css

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../../assets/css/footer.css';
+
+function Footer(props) {
+
+    return(
+        <section className="footer-section fixed-bottom">
+                <footer className="">
+                    <p className="footer-text col-12">designed and built by Christopher Wolfe <i class="fa fa-copyright" aria-hidden="true">©2021</i></p>
+                </footer>
+        </section>
+    )
+}
+
+export default Footer;
