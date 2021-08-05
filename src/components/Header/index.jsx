ff60bb50528ab211dc39f39b8298eed41ee05e24
Chrisdocs <chrisa.wolfe@gmail.com>
2021-08-05 16:38:02 -0500

    fixed screen sizing

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,14 +5,17 @@ import Navigation from '../Navigation';
 function Header(props) {
 
     return (
-        <div className="container header-box">
-            <div className="row align-items-center">
+        <div className="container">
+            <div className="row align-items-center header-box">
                     <div className="col-3">
                         <img className="wolf-logo" src={require('../../assets/img/wolf-logo2.png').default} alt={"wolf and moon logo"} />
                     </div>
-                    <div className="col-8">
+                    <div className="col-6">
                         <a href="/" className="header-title-link">{`{Wolfe}`} Web.Development
                         </a>
+                    </div>
+                    <div className="col-3">
+                        <img className="wolf-logo" src={require('../../assets/img/wolf-logo3.png').default} alt={"wolf and moon logo"} />
                     </div>
             </div>
         <Navigation></Navigation>
