99ed3c74034bdf0d247eadf59eef5bcd95e91b19
Chrisdocs <chrisa.wolfe@gmail.com>
2021-08-06 14:54:28 -0500

    fixed issue iwth footer covering component content

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,8 +5,23 @@ function Footer(props) {
 
     return(
         <section className="footer-section fixed-bottom">
-                <footer className="">
+                <footer>
+
+
+                    <a href="https://www.codewars.com/users/Chrisdocs">
+                    <img className="footer-img" src={require('../../assets/img/codewars-icon.png').default}/>
+                    </a>
+
+                    <a href="https://github.com/Chrisdocs">
+                    <img className="footer-img" src={require('../../assets/img/github-icon.png').default}/>
+                    </a>
+
+                    <a href="https://www.linkedin.com/in/christopher-wolfe-40762580/">
+                    <img className="footer-img" src={require('../../assets/img/linkedin-icon.png').default}/>
+                    </a>
+
                     <p className="footer-text col-12">designed and built by Christopher Wolfe <i className="fa fa-copyright" aria-hidden="true">©2021</i></p>
+
                 </footer>
         </section>
     )
