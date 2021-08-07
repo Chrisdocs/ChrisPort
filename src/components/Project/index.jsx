5973639d8cf06c802da01f517fc2085634ebf870
Chrisdocs <chrisa.wolfe@gmail.com>
2021-08-07 15:26:44 -0500

    carousel functionality using react nulti carousel npm, added 2 more projects and descriptions

--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 import '../../assets/css/project.css';
 import runBuddyImg from '../../assets/img/run-snip.png';
 import knowGoImg from '../../assets/img/knowgo.png';
 import chatnLearnImg from '../../assets/img/chat.png';
 import socialImg from '../../assets/img/social.png';
+import readmeImg from '../../assets/img/readmegen.png';
+import taskImg from '../../assets/img/schImg.png';
 
 function Project(props) {
 
@@ -14,7 +18,7 @@ function Project(props) {
             projectLink: "https://chrisdocs.github.io/run-buddy/",
             projectImg: runBuddyImg,
             imageAlt: "Snippet of the Run Buddy app.",
-            projectText: "lorem ipsum..."
+            projectText: "A sample web App for conencting with personal trainers!"
         },
         {
             id: 2,
@@ -22,7 +26,7 @@ function Project(props) {
             projectLink: "https://chrisdocs.github.io/KnowGo/",
             projectImg: knowGoImg,
             imageAlt: "snippet of the KnowGo app.",
-            projectText: "lorem ipsum..."
+            projectText: "Find out weather and local information before you go!"
         },
         {
             id: 3,
@@ -30,7 +34,7 @@ function Project(props) {
             projectLink: "https://chatnlearn.herokuapp.com/",
             projectImg: chatnLearnImg,
             imageAlt: "Snippet of the Chat and learn app.",
-            projectText: "lorem ipsum..."
+            projectText: "Chat with users while learning each others language!"
         },
         {
             id: 4,
@@ -38,9 +42,45 @@ function Project(props) {
             projectLink: "https://github.com/Chrisdocs/SocNetAPI",
             projectImg: socialImg,
             imageAlt: "image which reads Social Media Api in bold font.",
-            projectText: "lorem ipsum..."
+            projectText: "An API for backend user management!"
         },
+        {
+            id: 5,
+            projectName: "Readme Generator",
+            projectLink: "https://github.com/Chrisdocs/Readme-Gen",
+            projectImg: readmeImg,
+            imageAlt: "reads Read me generator in bold font.",
+            projectText: "Terminal program to help generate those posky readme.md files!"
+        },
+        {
+            id: 6,
+            projectName: "Task Scheduler",
+            projectLink: "https://chrisdocs.github.io/calendar/",
+            projectImg: taskImg,
+            imageAlt: "A snippet of the task scheduler web-app",
+            projectText: "A simple web app for managing your day to day tasks."
+        }
     ];
+
+    const responsive = {
+        superLargeDesktop: {
+            // the naming can be any, depends on you.
+            breakpoint: { max: 4000, min: 3000 },
+            items: 5,
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 3,
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 2,
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1,
+        },
+    };
 
 
     return (
@@ -56,7 +96,7 @@ function Project(props) {
                     </p>
                 </div>
 
-                <div className="flex flex-wrap -m-4">
+                {/* <div className="flex flex-wrap -m-4">
                     {projects.map((project, id) => (
                         <section key={project.id} className="project-map-section">
                             <div className="project-div card" key={project.id}>
@@ -74,8 +114,109 @@ function Project(props) {
                             </div>
                         </section>
                     ))}
-                </div>
+                </div> */}
 
+            </div>
+
+            <div>
+                <Carousel responsive={responsive}>
+                        <div>
+                            <div className="card">
+                                <div>
+                                    <img className="card-img-top" src={projects[0].projectImg}></img>
+                                </div>
+                                <div className="card-body">
+                                    <div className="title-div">
+                                        <a className="project-title card-title" href={projects[0].projectLink}>{projects[0].projectName}</a>
+                                    </div>
+                                    <div>
+                                        <p className="project-text card-text">{projects[0].projectText}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div>
+                            <div className="card">
+                                <div>
+                                    <img className="card-img-top" src={projects[1].projectImg}></img>
+                                </div>
+                                <div className="card-body">
+                                    <div className="title-div">
+                                        <a className="project-title card-title" href={projects[1].projectLink}>{projects[1].projectName}</a>
+                                    </div>
+                                    <div>
+                                        <p className="project-text card-text">{projects[1].projectText}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div>
+                            <div className="card">
+                                <div>
+                                    <img className="card-img-top" src={projects[2].projectImg}></img>
+                                </div>
+                                <div className="card-body">
+                                    <div className="title-div">
+                                        <a className="project-title card-title" href={projects[2].projectLink}>{projects[2].projectName}</a>
+                                    </div>
+                                    <div>
+                                        <p className="project-text card-text">{projects[2].projectText}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div>
+                            <div className="card">
+                                <div>
+                                    <img className="card-img-top" src={projects[3].projectImg}></img>
+                                </div>
+                                <div className="card-body">
+                                    <div className="title-div">
+                                        <a className="project-title card-title" href={projects[3].projectLink}>{projects[3].projectName}</a>
+                                    </div>
+                                    <div>
+                                        <p className="project-text card-text">{projects[3].projectText}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div>
+                            <div className="card">
+                                <div>
+                                    <img className="card-img-top" src={projects[4].projectImg}></img>
+                                </div>
+                                <div className="card-body">
+                                    <div className="title-div">
+                                        <a className="project-title card-title" href={projects[4].projectLink}>{projects[4].projectName}</a>
+                                    </div>
+                                    <div>
+                                        <p className="project-text card-text">{projects[4].projectText}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div>
+                            <div className="card">
+                                <div>
+                                    <img className="card-img-top" src={projects[5].projectImg}></img>
+                                </div>
+                                <div className="card-body">
+                                    <div className="title-div">
+                                        <a className="project-title card-title" href={projects[5].projectLink}>{projects[5].projectName}</a>
+                                    </div>
+                                    <div>
+                                        <p className="project-text card-text">{projects[5].projectText}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                </Carousel>
             </div>
         </section>
     )
